71d6bd2f4ddbca1c0b36358adf7473ee291416b7
KeeganAriel <keegan.teinowitz@gmail.com>
2016-03-04 11:32:38 -0800

    check for NaN

--- main.js
+++ main.js
@@ -26,8 +26,12 @@ function fizzBuzz(inputFizz) {
 var inputFizz = prompt("Enter your number here! Let's FizzBuzz!");
 
 // JavaScript to convert a string into an integer.
-parseInt(inputFizz, 10);
-
+inputFizz = parseInt(inputFizz, 10);
+    
+// check for NaN
+    if (isNaN(inputFizz)) {
+        prompt("Are you sure that's a real number? Enter a number!");
+    }
 // check to see if it's a whole number between 1-100
 if (inputFizz > 100) {
     prompt("You need to enter a smaller number!");
@@ -38,11 +42,6 @@ if (inputFizz > 100) {
 fizzBuzz(inputFizz);
 
 }
-
-
-// no, then promt for real number
-    // prompt("Are you sure that's a real number?");
-
 
 // code to ensure that the user has not supplied a decimal value
 
