73ec84bb7312df853114850ff0b52404e47a4b76
KeeganAriel <keegan.teinowitz@gmail.com>
2016-03-07 11:13:17 -0800

    decimal returns true

--- validator.js
+++ validator.js
@@ -1,7 +1,11 @@
 function validInput(userString) {
     if (isNaN(userString)) {
     	return false;
-    } 
+    } else {
+    	return true;
+    }
 }
 console.log(validInput("one") === false); 
 console.log(validInput("33") === true); 
+console.log(validInput("pizza") === false); 
+console.log(validInput("3.14") === false); 
